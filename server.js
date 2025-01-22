ce500c5c04e8a7e8719142b338b455a376ede24c
Sam  Koolmees <101374@b5-420-10.ict.lab.locals>
2025-01-22 14:08:10 +0100

    node test

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');       
+const app = express();              
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/galerij', (req, res) => {
+    res.sendFile(path.join(__dirname, 'galerij.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+// Start de server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
