284f7dd7b11d516dc6ebcbd6c09141c89dbcee7a
Sam  Koolmees <101374@b5-420-10.ict.lab.locals>
2025-01-22 14:41:18 +0100

    url test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 
 
-app.get('/', (req, res) => {
+app.get('/nodetest', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
